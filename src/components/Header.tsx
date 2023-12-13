df9c0ccba296785f4cd2f8a095164211fe0dc161
Hélvio Filho <helviosvf@gmail.com>
2023-12-13 19:01:14 -0300

    feat: created Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { IconProps } from "phosphor-react-native";
+type HeaderProps = {
+  title: string;
+  subtitle: string;
+  onPress: () => void;
+  icon: React.FC<IconProps>;
+};
+export function Header({ title, subtitle, onPress, icon: Icon }: HeaderProps) {
+  return (
+    <View className="w-full bg-grey-600 flex-row items-center justify-between px-8 pt-14 pb-6">
+      <View>
+        <Text className="text-2xl font-bold text-grey-100">{title}</Text>
+        <Text className="text-sm font-regular text-grey-100">{subtitle}</Text>
+      </View>
+
+      <TouchableOpacity
+        className="w-11 h-11 rounded-md items-center justify-center bg-grey-800"
+        activeOpacity={0.8}
+        onPress={onPress}
+      >
+        <Icon size={28} color="#E1E1E6" />
+      </TouchableOpacity>
+    </View>
+  );
+}
