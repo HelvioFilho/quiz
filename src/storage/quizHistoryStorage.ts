98985b3ab299b265d82117b426b4ffcf14494cda
Hélvio Filho <helviosvf@gmail.com>
2023-12-13 22:31:08 -0300

    feat: created history add and getAll functions

---
+++ src/storage/quizHistoryStorage.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
+
+type HistoryProps = {
+  id: string;
+  title: string;
+  points: number;
+  questions: number;
+  level: number;
+};
+
+const HISTORY_COLLECTION = "@quiz:history";
+
+export async function historyGetAll() {
+  try {
+    const storage = await AsyncStorage.getItem(HISTORY_COLLECTION);
+    const history: HistoryProps[] = storage ? JSON.parse(storage) : [];
+
+    return history;
+  } catch (error) {
+    Alert.alert("Opa", "Não foi possível obter o histórico.");
+
+    return [];
+  }
+}
+
+export async function historyAdd(newHistory: HistoryProps) {
+  try {
+    const response = await historyGetAll();
+    const storedHistory = response ? response : [];
+
+    const storage = JSON.stringify([...storedHistory, newHistory]);
+
+    await AsyncStorage.setItem(HISTORY_COLLECTION, storage);
+  } catch (error) {
+    throw error;
+  }
+}
