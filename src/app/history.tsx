79f1c3e2333039c312fa98eeff49ccbb1d37fd52
Hélvio Filho <helviosvf@gmail.com>
2023-12-15 12:07:55 -0300

    feat: created history screen

--- src/app/history.tsx
+++ src/app/history.tsx
@@ -1,9 +1,77 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, TouchableOpacity, View } from "react-native";
+import { useRouter } from "expo-router";
+import { HouseLine } from "phosphor-react-native";
 
+import { Header } from "@/components/Header";
+import { HistoryCard } from "@/components/HistoryCard";
+import { Loading } from "@/components/Loading";
+
+import { historyGetAll, historyRemove } from "@/storage/quizHistoryStorage";
+
+export type HistoryProps = {
+  id: string;
+  title: string;
+  points: number;
+  questions: number;
+  level: number;
+};
 export default function history() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [history, setHistory] = useState<HistoryProps[]>([]);
+
+  const navigation = useRouter();
+
+  async function fetchHistory() {
+    const response = await historyGetAll();
+    setHistory(response);
+    setIsLoading(false);
+  }
+
+  async function remove(id: string) {
+    await historyRemove(id);
+    fetchHistory();
+  }
+
+  function handleRemove(id: string) {
+    Alert.alert("Remover", "Deseja remover esse registro?", [
+      {
+        text: "Sim",
+        onPress: () => remove(id),
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <View>
-      <Text>History</Text>
+    <View className="flex-1 bg-grey-800">
+      <Header
+        title="Histórico"
+        subtitle={`Seu histórico de estudos${"\n"}realizados`}
+        icon={HouseLine}
+        onPress={() => navigation.back()}
+      />
+      <ScrollView
+        contentContainerStyle={{ padding: 32 }}
+        showsVerticalScrollIndicator={false}
+      >
+        {history.map((item) => (
+          <TouchableOpacity key={item.id} onPress={() => handleRemove(item.id)}>
+            <HistoryCard data={item} />
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
