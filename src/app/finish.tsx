44b9b5b146ca6920b114f26741f7f7f797116e2b
Hélvio Filho <helviosvf@gmail.com>
2023-12-15 11:47:02 -0300

    feat: created finish screen

--- src/app/finish.tsx
+++ src/app/finish.tsx
@@ -1,9 +1,26 @@
 import { Text, View } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { Button } from "@/components/Button";
+
+type FinishParams = {
+  points: string;
+  total: string;
+};
 
 export default function finish() {
+  const { points, total } = useLocalSearchParams() as FinishParams;
+  const navigation = useRouter();
   return (
-    <View>
-      <Text>Finish</Text>
+    <View className="flex-1 justify-center bg-grey-800 p-8">
+      <View className="items-center mb-20">
+        <Text className="font-bold text-2xl text-grey-100 mt-10">
+          Parabéns!
+        </Text>
+        <Text className="font-regular text-base text-grey-100 mt-2">
+          Você acertou {points} de {total} questões.
+        </Text>
+        <Button title="Ir para o início" onPress={() => navigation.push("/")} />
+      </View>
     </View>
   );
 }
