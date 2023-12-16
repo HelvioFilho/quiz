bb839735b8e4f67c3fa755be45225dd9681f464e
Hélvio Filho <helviosvf@gmail.com>
2023-12-16 11:04:56 -0300

    feat: animating the progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 type ProgressBarProps = {
   total: number;
@@ -7,11 +13,24 @@ type ProgressBarProps = {
 
 export function ProgressBar({ total, current }: ProgressBarProps) {
   const percentage = Math.round((current / total) * 100);
+
+  const sharedProgress = useSharedValue(0);
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage, { duration: 700 });
+  }, [current]);
+
   return (
     <View className="h-2 w-full rounded-lg bg-grey-500">
-      <View
+      <Animated.View
         className="h-2 rounded-lg bg-brand-light"
-        style={{ width: `${percentage}%` }}
+        style={styleAnimated}
       />
     </View>
   );
