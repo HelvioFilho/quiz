f716742b4e5135f131bf22b575d9b5241bb971a3
Hélvio Filho <helviosvf@gmail.com>
2023-12-18 15:19:02 -0300

    feat: creating animation of the stars around the trophy

--- src/app/finish.tsx
+++ src/app/finish.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { Button } from "@/components/Button";
+import { Stars } from "@/components/Stars";
 
 type FinishParams = {
   points: string;
@@ -13,14 +14,15 @@ export default function finish() {
   return (
     <View className="flex-1 justify-center bg-grey-800 p-8">
       <View className="items-center mb-20">
+        <Stars />
         <Text className="font-bold text-2xl text-grey-100 mt-10">
           Parabéns!
         </Text>
         <Text className="font-regular text-base text-grey-100 mt-2">
           Você acertou {points} de {total} questões.
         </Text>
-        <Button title="Ir para o início" onPress={() => navigation.push("/")} />
       </View>
+      <Button title="Ir para o início" onPress={() => navigation.push("/")} />
     </View>
   );
 }
